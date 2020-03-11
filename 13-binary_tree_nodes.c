47252880faabc20e4b32f5e70a6f00d59f6461f3
Christian David Suárez Guerra <darkdrakkar@gmail.com>
2020-03-11 15:25:12 -0500

    function to count number of nodes

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+/**
+ ** binary_tree_nodes - function that counts the nodes in a binary tree.
+ **
+ ** @tree: pointer to the root node of the tree to count the number of nodes.
+ ** Return: number of nodes.
+ **/
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	int nodes = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->right == NULL && tree->left == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		nodes += 1;
+		if (tree->parent == NULL)
+			nodes += 1;
+	}
+	nodes += binary_tree_nodes(tree->right);
+	return (nodes);
+}
